487dedc544a66cd4f80aad1c37145e0d808b25dc
lyssadev <toolsplus1000@gmail.com>
2025-07-10 04:00:35 +0000

    1.1.0

--- app/src/main/java/com/noxpeteam/txpacker/BuildConfig.kt
+++ app/src/main/java/com/noxpeteam/txpacker/BuildConfig.kt
@@ -1,8 +1,8 @@
 package com.noxpeteam.txpacker
 
 object BuildConfig {
-    const val VERSION_NAME = "1.0.5"
-    const val VERSION_CODE = 6
+    const val VERSION_NAME = "1.1.0"
+    const val VERSION_CODE = 7
     const val APPLICATION_ID = "com.noxpeteam.txpacker"
     const val BUILD_TYPE = "release"
 }
